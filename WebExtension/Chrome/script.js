ec9915682068d9e013127e3201a6d9d4d62db38b
Mikkel Malmberg <mikkel@brnbw.com>
2019-04-01 20:03:21 +0200

    Add Chrome extension

---
+++ WebExtension/Chrome/script.js
@@ -0,0 +1,23 @@
+function apply(script) {
+  if (document.body) {
+    if (script) {
+      try {
+        eval(script);
+      } catch (e) {
+        console.groupCollapsed('%cjoof failed eval\'ing your script', 'color:red')
+        console.error(e)
+        console.groupEnd()
+      }
+    }
+  } else {
+    setTimeout(function() {
+      apply(script);
+    }, 200);
+  }
+}
+
+const filename = location.hostname.replace(/^www\./, "") + ".js";
+
+fetch('https://localhost:3133/' + filename)
+  .then(resp => resp.text())
+  .then(apply)
