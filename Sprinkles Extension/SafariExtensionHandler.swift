6c5be0a228a2c562dce88a180bbda436faeba105
Mikkel Malmberg <mikkel@brnbw.com>
2020-08-03 20:40:58 +0200

    Bump build numbers

--- Sprinkles Extension/SafariExtensionHandler.swift
+++ Sprinkles Extension/SafariExtensionHandler.swift
@@ -29,54 +29,54 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
       completionHandler: nil)
   }
 
-  override func messageReceived(
-    withName messageName: String, from page: SFSafariPage, userInfo: [String: Any]? = nil
-  ) {
-    // If we've already failed at something, we ignore new messages
-    if state == .requestFailed || state == .evalFailed { return }
+  //  override func messageReceived(
+  //    withName messageName: String, from page: SFSafariPage, userInfo: [String: Any]? = nil
+  //  ) {
+  //    // If we've already failed at something, we ignore new messages
+  //    if state == .requestFailed || state == .evalFailed { return }
+  //
+  //    state = stateFromMessage(messageName)
+  //    updateToolbarItemFor(page: page)
+  //  }
+  //
+  //  private func stateFromMessage(_ messageName: String) -> State {
+  //    switch messageName {
+  //    case "ok": return .ok
+  //    case "request-failed": return .requestFailed
+  //    case "eval-failed": return .evalFailed
+  //    default: return .unknown
+  //    }
+  //  }
+  //  private func updateToolbarItemFor(page: SFSafariPage) {
+  //    itemFor(page: page) { (maybeItem) in
+  //      guard let item = maybeItem else { return }
+  //
+  //      item.setLabel(nil)
+  //      item.setEnabled(true)
+  //      item.setBadgeText(nil)
+  //
+  //      switch self.state {
+  //      case .requestFailed:
+  //        item.setEnabled(false)
+  //        item.setBadgeText("!")
+  //        item.setLabel("Sprinkles failed to request scripts. Server down?")
+  //        break
+  //      case .evalFailed:
+  //        item.setBadgeText("!")
+  //        item.setLabel("Sprinkles failed running your scripts. See console for more.")
+  //        break
+  //      case .unknown:
+  //        item.setBadgeText("?")
+  //      default: break
+  //      }
+  //    }
+  //  }
 
-    state = stateFromMessage(messageName)
-    updateToolbarItemFor(page: page)
-  }
-
-  private func stateFromMessage(_ messageName: String) -> State {
-    switch messageName {
-    case "ok": return .ok
-    case "request-failed": return .requestFailed
-    case "eval-failed": return .evalFailed
-    default: return .unknown
-    }
-  }
-  private func updateToolbarItemFor(page: SFSafariPage) {
-    itemFor(page: page) { (maybeItem) in
-      guard let item = maybeItem else { return }
-
-      item.setLabel(nil)
-      item.setEnabled(true)
-      item.setBadgeText(nil)
-
-      switch self.state {
-      case .requestFailed:
-        item.setEnabled(false)
-        item.setBadgeText("!")
-        item.setLabel("Sprinkles failed to request scripts. Server down?")
-        break
-      case .evalFailed:
-        item.setBadgeText("!")
-        item.setLabel("Sprinkles failed running your scripts. See console for more.")
-        break
-      case .unknown:
-        item.setBadgeText("?")
-      default: break
-      }
-    }
-  }
-
-  private func itemFor(page: SFSafariPage, handler: @escaping (SFSafariToolbarItem?) -> Void) {
-    page.getContainingTab { (tab) in
-      tab.getContainingWindow { (window) in
-        window?.getToolbarItem(completionHandler: handler)
-      }
-    }
-  }
+  //  private func itemFor(page: SFSafariPage, handler: @escaping (SFSafariToolbarItem?) -> Void) {
+  //    page.getContainingTab { (tab) in
+  //      tab.getContainingWindow { (window) in
+  //        window?.getToolbarItem(completionHandler: handler)
+  //      }
+  //    }
+  //  }
 }
